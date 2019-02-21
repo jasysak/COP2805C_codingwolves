a59375b6d54a4ea043eaaf5786c509027bb70e12
jasysak <jsysak@hawkmail.hccfl.edu>
2019-02-21 11:34:08 -0500

    added getFileChecksum method

--- src/main/java/view/codingwolves/ParseFile.java
+++ src/main/java/view/codingwolves/ParseFile.java
@@ -4,7 +4,11 @@
 package view.codingwolves;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,23 +20,34 @@ public class ParseFile {
 
 	/**
 	 * @param args
+	 * @throws NoSuchAlgorithmException 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		Scanner s = null;
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+
 		String filepath = "./src/main/resources/"; // this will need to be updated...
 		
 		// temporarily hard-set filename -- this will be user entry
 		String filename = "Test_Sample2.txt";
+
+		//Create checksum for this file
+		File file = new File(filepath + filename);
+		 
+		//Use MD5 algorithm
+		MessageDigest md5Digest = MessageDigest.getInstance("MD5");
+		 
+		//Get the checksum
+		String checksum = getFileChecksum(md5Digest, file);
+		 
+		//see checksum
+		System.out.println(checksum);
 		
-		try {
-			s = new Scanner(new File(filepath + filename));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Scanner s = new Scanner(new File(filepath + filename));
+		
 		ArrayList<String> list = new ArrayList<String>();
 		while (s.hasNext()){
 		    list.add(s.next());
+		    
 		}
 		s.close();
 		
@@ -48,6 +63,43 @@ public class ParseFile {
 		// Work in progress...
 		//
 		
+	}
+	
+	/*
+	 * Method getFileChecksum taken from 
+	 * https://howtodoinjava.com/java/io/how-to-generate-sha-or-md5-file-checksum-hash-in-java/
+	 * by L. Gupta
+	 */
+	private static String getFileChecksum(MessageDigest digest, File file) throws IOException
+	{
+	    //Get file input stream for reading the file content
+	    FileInputStream fis = new FileInputStream(file);
+	     
+	    //Create byte array to read data in chunks
+	    byte[] byteArray = new byte[1024];
+	    int bytesCount = 0;
+	      
+	    //Read file data and update in message digest
+	    while ((bytesCount = fis.read(byteArray)) != -1) {
+	        digest.update(byteArray, 0, bytesCount);
+	    };
+	     
+	    //close the stream; We don't need it now.
+	    fis.close();
+	     
+	    //Get the hash's bytes
+	    byte[] bytes = digest.digest();
+	     
+	    //This bytes[] has bytes in decimal format;
+	    //Convert it to hexadecimal format
+	    StringBuilder sb = new StringBuilder();
+	    for(int i=0; i< bytes.length ;i++)
+	    {
+	        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+	    }
+	     
+	    //return complete hash
+	   return sb.toString();
 	}
     		
 }
