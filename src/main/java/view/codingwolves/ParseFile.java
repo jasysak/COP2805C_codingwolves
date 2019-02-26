4f3775e5db71eb57aa351a83bb890b53db4ee094
jasysak <jsysak@hawkmail.hccfl.edu>
2019-02-26 00:24:58 -0500

    Cleaned up code a bit. Inverted index only half working

--- src/main/java/view/codingwolves/ParseFile.java
+++ src/main/java/view/codingwolves/ParseFile.java
@@ -1,10 +1,24 @@
 /**
+ * @author Jason Sysak
+ * @version 1.0.0 
  * 
+ * Feb. 25, 2019
+ * 
+ * For COP2805C Group Project -- Project #4
+ * 
+ * codingwolves team
+ * Members:
+ * David A.
+ * Reubin G.
+ * Erin H.
+ * Jason S.
+ *
  */
 package view.codingwolves;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -25,22 +39,14 @@ import org.apache.commons.collections4.MapUtils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-
-
-//import javax.json.*;
-
-/**
- * @author jasysak
- *
- */
 public class ParseFile {
 
 	/**
 	 * @param args
 	 * @throws NoSuchAlgorithmException 
 	 * @throws IOException 
-	 */
-	 /*
+	 *
+	 *
 	 *  This is the basic inverted index model: Map<Word_In_File, SortedSet<FileID, WordPosition>>
 	 *  where <FileID, WordPosition> will be an object of type FilePosition - see FilePosition.class
 	 *  So an example for the word "The" may be: 
@@ -50,6 +56,7 @@ public class ParseFile {
 	 *  this creates the basis of the inverted index, where each file is parsed into the Map (or removed
 	 *  from the Map in the case of file removal).
 	 *  
+	 *  WORK IN PROGRESS
 	 */ 
 
 	static Map<String, SortedSet<FilePosition>> mainIndex = new HashMap<String, SortedSet<FilePosition>>();
@@ -65,16 +72,44 @@ public class ParseFile {
 		//Get & print checksum
 		String checksum = getFileChecksum(md5Digest, new File(filepath + filename));
 		System.out.println(checksum);
-		//
-		// main file read and mainIndex construction begins here
-		//
-		Scanner s = new Scanner(new File(filepath + filename));
+		
+		// simple call to method 
+		// for now it returns the ArrayList list
+		// only to build the JSON data below
+		ArrayList<String> list = addToIndex(filepath, filename);
+		
+		// create & print json formatted string from ArrayList(list)
+		String json = new Gson().toJson(list);
+		//System.out.println(json);
+				
+		// now create the "full" json object and pretty print with gson
+		JsonObject value = Json.createObjectBuilder()
+			.add("filename", filename)
+			.add("checksum", checksum)
+			.add("path", filepath)
+			.add("Parsed Data", json)
+			.build();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json_obj = gson.toJson(value);
+		System.out.println(json_obj);
 			
+	} // end main
+	
+	// method addToIndex 
+	// main file read and mainIndex construction begins here
+	//
+	static ArrayList<String> addToIndex(String filepath, String filename) throws FileNotFoundException {
+		Scanner s = new Scanner(new File(filepath + filename));
+		
 		long fileID = 1; // placeholder fileID until fileID generation is decided
 		// position will contain the current word position in the file as it's read
 		int position = 0;
 		
 		// I've left the ArrayList in place, but may not be needed moving forward
+		// it is only used for creating the JSON data, and placed here within the
+		// addToIndex method since it uses the scanner while loop below to read the
+		// file contents into the ArrayList. Note this method must return therefore
+		// return the ArrayList to main.
 		ArrayList<String> list = new ArrayList<String>();
 		
 		// loop to read file contents
@@ -89,7 +124,8 @@ public class ParseFile {
 			list.add(word);
 			// create a new fileposition object "fp" that contains the fileID (long), current word position (int)
 			FilePosition fp = new FilePosition(fileID, position);
-			// if the mainIndex does not contain "word" then we add it
+			// if the mainIndex does not contain "word" then we add the word along with a new
+			// TreeSet containing a single object fp
 			if (!mainIndex.containsKey(word)) {
 				// this code seems to work. Adds new fp if word doesn't exist yet in mainIndex
 				SortedSet<FilePosition> newWordPosition = new TreeSet<FilePosition>();
@@ -98,49 +134,45 @@ public class ParseFile {
 				System.out.println(word + " at " + fp.toString() + " ADDED");
 			}
 			// if the mainIndex already contains word, then we add only the fp object
-			// at the existingWordPosition
+			// to an existingWordPosition Set
 			// this is not working as expected...
 			else {
-				SortedSet<FilePosition> existingWordPosition = new TreeSet<FilePosition>();
-				existingWordPosition = mainIndex.get(word);
-				System.out.println("Before ADD \n" + existingWordPosition.toString());
+				// test contents of fp
+				System.out.println("File ID = " + fp.fileID);
+				System.out.println("position = " + fp.wordposition);
+				// Note: the above test confirms the correct fileID and position values
+				// but the lines below may not be adding this fp object to the existingWordPosition
+				// Set ??? I don't know why...yet
+				Set<FilePosition> existingWordPosition = mainIndex.get(word);
 				existingWordPosition.add(fp);
-				System.out.println("After ADD \n" + existingWordPosition.toString());
-				//System.out.println(fp.fileID);
-				//System.out.println(fp.wordposition);
-				mainIndex.put(word, existingWordPosition);		
+				// the following iterator should print the entire contents of existingWordPosition
+				// Set, but it is only printing the first fp added to it. Which leads me to believe
+				// again that the above code is not adding the last fp object to the Set
 				Iterator<FilePosition> it = existingWordPosition.iterator();
 			     while(it.hasNext()){
 			        System.out.println(it.next());
 			     }
-				
-				// NOTE for above: there is a lot of duplicated code. This is not
-			    // done yet and will be re-factored once it works as intended...
+				 // On further thought, could the problem simply be the iterator is not
+			     // properly iterating through all fp objects in the Set? (that is, they
+			     // are there, just not being printed!?)
 			}
 			++position;
 		} // end while loop
 		s.close();
 		
 		// Print the map using Apache commons collection MapUtils	
+		// note that only one fp object is printed from each set
+		// for each word in the mainIndex Map (due to suspected bug in the
+		// else/"add fp to existing word" code section above).
 		//MapUtils.verbosePrint(System.out, "Inverted Index", mainIndex);
 		MapUtils.debugPrint(System.out, "DEBUG Print", mainIndex);
 		
-		// create & print json formatted string from ArrayList(list)
-		String json = new Gson().toJson(list);
-		System.out.println(json);
+		return list; // temporary return of list for JSON building
 		
-		// now create the "full" json object and pretty print with gson
-		JsonObject value = Json.createObjectBuilder()
-				.add("filename", filename)
-				.add("checksum", checksum)
-				.add("path", filepath)
-				.add("Parsed Data", json)
-				.build();
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String json_obj = gson.toJson(value);
-		System.out.println(json_obj);
+	} // end addToIndex
 		
-	}
+		
+	
 	
 	/*
 	 * Method getFileChecksum taken from 
