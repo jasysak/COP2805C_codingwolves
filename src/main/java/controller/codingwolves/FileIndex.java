a188cc0abd779a8e7d89f062807a4126a9fdba74
jasysak <jsysak@hawkmail.hccfl.edu>
2019-03-12 17:29:28 -0400

    Added inverted index basic build and save code
    The BASIC inverted index construction and saving is now in
    FileIndex.java. This is very much still a work in progress and I will be
    adding and modifying a lot over the next few days. Also the
    Test_Sample*.txt files have been placed in the resources folder. The
    inverted index JSON file will also be saved to this location.

--- src/main/java/controller/codingwolves/FileIndex.java
+++ src/main/java/controller/codingwolves/FileIndex.java
@@ -1,15 +1,22 @@
 package controller.codingwolves;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.TableView.TableViewSelectionModel;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -22,12 +29,29 @@ import view.codingwolves.MaintenanceWindow;
  * add files to the index, remove files from the index, update files in the index, AND search,
  * OR search, PHRASE search, and to save index to file.
  *
+ * also contains methods pertaining to the inverted index: build,
+ * update, edit, file removal, etc.
+ * 
  * @author David Alvarez, 2/24/19
+ * @author Jason Sysak
+ * 
+ * For COP2805C Group Project
+ * 
+ * codingwolves team
+ * Members:
+ * David A.
+ * Reubin G.
+ * Erin H.
+ * Jason S.
  *
+ * WORK IN PROGRESS
+ * 
  */
+
 public class FileIndex {
 	static FileModel model = new FileModel();
 	static String fileName;
+	static Map<String, SortedSet<FilePosition>> mainIndex = new HashMap<String, SortedSet<FilePosition>>();
 	
 	public static void andSearch() {
 		
@@ -61,6 +85,7 @@ public class FileIndex {
 			return;
 		}
 		model.addFile(fileName);
+		addToIndex(fileName);
 	}
 	//Still a work in progress
 	public static void updateFilesInIndex()	{
@@ -94,4 +119,97 @@ public class FileIndex {
 			model.removeFile(fileId);
 		}
 	}
+	
+	static void addToIndex(String filename) throws FileNotFoundException, IOException {
+		
+		Scanner s = new Scanner(new File(filename));
+		
+		// placeholder fileID
+		long fileID = 0;
+		
+		// position will contain the current word position in the file as it's read
+		int position = 0;
+	
+		while (s.hasNext()){	
+			String word = s.next().toLowerCase();
+			word = word.replaceAll("[^a-zA-Z0-9]", "");
+			if (word.length() == 0)
+				continue;
+			FilePosition fp = new FilePosition(fileID, position);
+			if (!mainIndex.containsKey(word)) {
+				SortedSet<FilePosition> newWordPosition = new TreeSet<FilePosition>();
+				newWordPosition.add(fp);
+				mainIndex.put(word, newWordPosition);
+			}
+
+			else {
+				SortedSet<FilePosition> existingWordPosition = mainIndex.get(word);
+				existingWordPosition.add(fp);
+				// some output to verify:
+				//System.out.print("KEY = " + String.format(("%-15s"), word) + ": ");
+				//Iterator<FilePosition> it = existingWordPosition.iterator();
+			    //System.out.print("VALUES = ");
+				//while(it.hasNext()){
+			    // 		System.out.print(it.next() + ", ");
+			    // }
+			    //System.out.println();
+			}
+			++position;
+		} // end while loop
+		s.close();
+		
+		// Print map using Apache commons collection MapUtils
+		// MapUtils.verbosePrint(System.out, "Inverted Index", mainIndex);
+		// MapUtils.debugPrint(System.out, "DEBUG Print", mainIndex);
+		
+		return;
+		
+	} // end addToIndex
+	
+	static void saveIndexToStorage (String iFilename) {
+		// I left setPrettyPrinting so the JSON output is nicer to
+		// read. This can be removed whenever it's no longer needed.
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json_obj = gson.toJson(mainIndex);
+		System.out.println("\nSaved JSON Data to " + iFilename + ":");
+		System.out.println(json_obj);
+			
+		try {
+			FileWriter writer = new FileWriter(iFilename);
+			writer.write(json_obj);
+			writer.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}	
+	}
+	static void removeDocumentFromIndex () {
+		// TODO stub method
+		// called when document is to be entirely removed from mainIndex
+		// Note that it will remain in the saved index JSON file until 
+		// program exit or manual Update Index (see below) is selected
+	}
+	
+	static void updateIndex() {
+		// TODO stub method
+		// called when "Update Index" button is pressed from Maintenance window
+	}
+	
+	static void loadIndexFromStorage () {
+		// TODO stub method
+		// called at startup to access disk storage of index and load it into memory/mainIndex Map
+	}
+	
+	
+	// main method added for TEST ONLY (this will be removed after bugs etc. are gone)
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+		
+		// placeholder filename 
+		String filename = "./src/main/resources/Test_Sample2.txt";
+		String indexFilename = "./src/main/resources/testfile.json";
+		
+		addToIndex(filename);
+		saveIndexToStorage(indexFilename);
+		
+	} // end TEST main method
 }
---
+++ src/main/java/controller/codingwolves/FilePosition.java
@@ -0,0 +1,88 @@
+/**
+ * @author Jason Sysak
+ * @version 1.0.0 
+ * 
+ * Feb. 25, 2019
+ * 
+ * For COP2805C Group Project
+ * 
+ * codingwolves team
+ * Members:
+ * David A.
+ * Reubin G.
+ * Erin H.
+ * Jason S.
+ *
+ * FilePosition.java (name subject to change)
+ * 
+ * Java class defining the FilePosition object to be used
+ * in the Inverted Index Map->Set value for each key "word"
+ *
+ */
+package controller.codingwolves;
+
+public class FilePosition implements Comparable<FilePosition>{
+
+	final long fileID;
+	final int wordposition;
+	
+	// Object constructor
+	
+	public FilePosition(long fileID, int wordposition) {
+		this.fileID = fileID;
+		this.wordposition = wordposition;
+	}
+	
+	// Override methods
+	
+	@Override
+	public int compareTo(FilePosition o) {
+		if ( o == null )
+			throw new IllegalArgumentException( "Argument must not be null." );
+		if (this.fileID == o.fileID)
+			// this will return the position offset
+			// between the positions of matched word
+			// in a set. That is, if the this.wordposition
+			// and o.wordposition are in fact different
+			// instances of the same word, the return
+			// value will be a positive, non-zero int
+		    return (this.wordposition - o.wordposition);
+		else
+			// this will return the fileID offset between
+			// two different files. Also a positive, non-zero
+			// int for different files in the index.
+			return ((int)(this.fileID - o.fileID));
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null) {
+			return false;
+		}
+ 
+		// this instance check
+		if (this == o) {
+			return true;
+		}
+		//
+		// TODO work in progress
+		//
+		return false;
+		
+	}
+	@Override
+	public int hashCode() {
+		int result = 17;
+		result = 37 * result + (int)fileID;
+		result = 37 * result + wordposition;
+		return result;
+	}
+	
+	@Override
+	public String toString() {
+	    return "fileID = " + this.fileID + " position = " + this.wordposition;
+	}
+	
+	
+	
+}
